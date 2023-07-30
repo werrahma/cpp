d176641e48fa865a943f6f5092c4151242d3f3e6
Walid Errahmani <werrahma@e1r12p6.1337.ma>
2023-07-30 21:39:34 +0100

    push

--- ex00/ex00.cpp
+++ ex00/ex00.cpp
@@ -3,25 +3,19 @@
 
 int main (int ac, char **av)
 {
-    int i;
-    int j;
-
-    i = 1;
     if (ac < 2)
     {
         std :: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
         return (0);
     }
-    while (av[i])
+    for (int i = 1; av[i]; i++)
     {
-        j = 0;
-        while (av[i][j])
+        for (int j = 0; av[i][j]; j++)
         {
             if (av[i][j] >= 'a' && av[i][j] <= 'z')
                 av[i][j] = toupper(av[i][j]);
             std :: cout << av[i][j];
-            j++;
         }
-        i++;
     }
+    std :: cout << std :: endl;
 }
