3be458779acd139115ef4e377ebd90ec7ebb94f8
werrahma <errahmaniwalid15@gmail.com>
2024-01-08 13:29:30 +0100

    module07

--- module08/ex01/main.cpp
+++ module08/ex01/main.cpp
@@ -1,21 +1,21 @@
 #include "Span.hpp"
 
-int main()
-{
-    Span sp = Span(5);
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
-    std::cout << "------------------------------------" << std::endl;
-    Span obj(15000);
-    for (int i = 0; i <= 10000; i++)
-    {
-        obj.addNumber(i);
-    }
-    std::cout << obj.shortestSpan() << std::endl;
-    std::cout << obj.longestSpan() << std::endl;;
-}
+// int main()
+// {
+//     Span sp = Span(5);
+//     sp.addNumber(6);
+//     sp.addNumber(3);
+//     sp.addNumber(17);
+//     sp.addNumber(9);
+//     sp.addNumber(11);
+//     std::cout << sp.shortestSpan() << std::endl;
+//     std::cout << sp.longestSpan() << std::endl;
+//     std::cout << "------------------------------------" << std::endl;
+//     Span obj(15000);
+//     for (int i = 0; i <= 10000; i++)
+//     {
+//         obj.addNumber(i);
+//     }
+//     std::cout << obj.shortestSpan() << std::endl;
+//     std::cout << obj.longestSpan() << std::endl;;
+// }
