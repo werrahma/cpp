ff61b9f134f9079865a18401008d37d5e9adfa3e
werrahma <errahmaniwalid15@gmail.com>
2024-03-16 22:28:59 +0000

    fix errors in moudle09/ex02

--- module09/ex02/PmergeMe.hpp
+++ module09/ex02/PmergeMe.hpp
@@ -3,24 +3,106 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstring>
+#include <deque>
+#include <ctime>
+#include <sys/time.h>
+
+const size_t K = 4;
+
+template<typename T>
 
 class PmergeMe
 {
-    std::vector<int> LA;
-    std::vector<int> RA;
-    // std::vector<std::pair<int, int> > pairs;
-    std::vector<int> vec;
-    int     tmp;
+    size_t     tmp;
     public:
-        // void    mergeInsetionSort();
-        void merge(std::vector<int>& A, int p, int q, int r);
-        void sort(std::vector<int>& A, int p, int r);
-        void insertionSort(std::vector<int>& A, int p, int q);
-        void    CreatePairs(std::vector<int> arr, std::vector<std::pair<int, int> > &pairs);
-        void    Psort(std::vector<int> &arr);
-};
-// void    merge(std::vector<int> &arr, int low, int high);
+        void merge(T& arr, size_t low, size_t mid, size_t high)
+        {
+            size_t n1 = mid - low + 1;
+            size_t n2 = high - mid;
 
+            // std::vector<size_t> left(n1), right(n2);  // Use vectors instead of copying subarrays
+            T left(n1);
+            T right(n2);
+            // Copy data to temporary arrays left[] and right[]
+            for (size_t i = 0; i < n1; i++)
+                left[i] = arr[low + i];
+            for (size_t j = 0; j < n2; j++)
+                right[j] = arr[mid + 1 + j];
+
+            // Merge the temporary arrays back size_to arr[low..high]
+            size_t i = 0, j = 0, k = low;
+            while (i < n1 && j < n2) {
+                if (left[i] <= right[j]) {
+                    arr[k] = left[i];
+                    i++;
+                }
+                else {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+            // Copy the remaining elements of left[], if any
+            while (i < n1) {
+                arr[k] = left[i];
+                i++;
+                k++;
+            }
+
+            // Copy the remaining elements of right[], if any
+            while (j < n2) {
+                arr[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+        void sort(T& arr, size_t low, size_t high)
+        {
+            if (high - low > K)
+            {
+                size_t mid = (low + high) / 2;
+                sort(arr, low, mid);
+                sort(arr, mid + 1, high);
+                merge(arr, low, mid, high);
+            }
+            else
+                insertionSort(arr,low, high);  // Use insertion sort for small subarrays
+        }
+        void insertionSort(T& arr, size_t low, size_t high)
+        {
+            for (size_t i = low; i < high; ++i) {
+                size_t tempVal = arr[i + 1];
+                size_t j = i + 1;
+                while (j > low && arr[j - 1] > tempVal) {
+                    arr[j] = arr[j - 1];
+                    j--;
+                }
+                arr[j] = tempVal;
+            }
+        }
+    T    removespaces(char **av)
+    {
+        T tmp;
+        for (size_t i = 1; av[i]; i++)
+        {
+            for (size_t j = 0; j < strlen(av[i]); j++)
+            {
+                if (!isdigit(av[i][j]) || av[i][j] == '-')
+                    throw (std::invalid_argument("Error"));
+            }
+            tmp.push_back(atoi(av[i]));
+        }
+        return tmp;
+    }
+    void    PrintArr(T arr)
+    {
+        for (size_t i = 0; i < arr.size(); i++) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
 
 
 #endif
--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -1,37 +1,42 @@
 #include "PmergeMe.hpp"
 #include <algorithm>
 
-int main()
+int main(int ac, char **av)
 {
     try
     {
-        // std::vector<int> vec;
-        // vec.push_back(15);
-        // vec.push_back(2);
-        // vec.push_back(5);
-        // vec.push_back(16);
-        // std::sort(vec.begin(), vec.end());
-        // for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++)
-        // {
-        //     std::cout << *it << std::endl;
-        // }
-        PmergeMe obj;
-        std::vector<int> A;
-        A.push_back(15);
-        A.push_back(2);
-        A.push_back(5);
-        A.push_back(16);
-        A.push_back(20);
-        // obj.sort(A, 0, A.size() - 1);
-        obj.Psort(A);
-        std::cout << ")))))))" << std::endl;
-        for (size_t i = 0; i < A.size(); i++) {
-            std::cout << A[i] << " ";
-        }
-        std::cout << std::endl;
+        if (ac <= 2)
+            throw (std::invalid_argument("Error"));
+        struct timeval start, end;
+        double dequeTime;
+        double vectorTime;
+
+        //// std::vector
+        PmergeMe<std::vector<size_t> > obj;
+        std::vector<size_t> vec = obj.removespaces(av);
+        std::cout << "Before:  "; obj.PrintArr(vec);
+        std::cout << "--- " << vec.size() << std::endl;
+        gettimeofday(&start, NULL);
+        obj.sort(vec, 0, vec.size() - 1);
+        // end = clock();
+        gettimeofday(&end, NULL);
+        vectorTime = (end.tv_sec - start.tv_sec) * 1000000.0;
+        vectorTime += (end.tv_usec - start.tv_usec);
+        std::cout << "After:   "; obj.PrintArr(vec);
+        std::cout << "Time to process a range of " << vec.size() << " elements with std::vector : " << vectorTime << " us" << std::endl;
+        //// std::deque
+        PmergeMe<std::deque<int> > obj2;
+        std::deque<int> deq = obj2.removespaces(av);
+        gettimeofday(&start, NULL);
+        obj.sort(vec, 0, vec.size() - 1);
+        gettimeofday(&end, NULL);
+        dequeTime = (end.tv_sec - start.tv_sec) * 1000000.0; // Convert seconds to microseconds
+        dequeTime += (end.tv_usec - start.tv_usec);
+        std::cout << "Time to process a range of " << vec.size() << " elements with std::deque : " << dequeTime << " us" << std::endl;
+        
     }
     catch (std::exception &e)
     {
-
+        std::cerr << e.what() << "\n";
     }
 }
